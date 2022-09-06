67e816a92485151d1678956c09dda5497961ab81
Martin Smith <martin.smith@microfocus.com>
2022-09-06 10:35:56 -0400

    try sample vuln

--- secrets.sh
+++ secrets.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-a=$1
-echo $1
-echo $a
-echo $1
-echo $1
+#!/bin/sh
+
+# Decrypt the file
+mkdir $HOME/secrets
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$TEST_REPO_NAME" \
+--output $HOME/secrets/my_secret.json my_secret.json.gpg
